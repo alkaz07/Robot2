b92ba60e2e1a9f1af116b174260eb185f2f2c34b
alkaz07 <alkaz07@yandex.ru>
2024-04-09 16:09:37 +0300

    сделали робота с памятью в виде множества уникальных элементов

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        if(runProg5("LSSSS") == -1)
+        if(runProg6("LSSSS") == -1)
             System.out.println("test 1 passed");
         else
             System.out.println("test 1 failed");
 
-        if(runProg5("SSLSLSLSSRSRS") == 5)
+        if(runProg6("SSLSLSLSSRSRS") == 5)
             System.out.println("test 2 passed");
         else
             System.out.println("test 2 failed");
 
-        if(runProg5("LSSSRSSSSRSSSSLSLLLL") == -1)
+        if(runProg6("LSSSRSSSSRSSSSLSLLLL") == -1)
             System.out.println("test 4 passed");
         else
             System.out.println("test 4 failed");
@@ -21,6 +21,14 @@ public class Main {
         int maxCountS = 10;
 
         Robot k79 = new Robot(maxCountS, maxCountS, 0);
+        int res = k79.runProgramUntilCross(prog);
+        return res;
+    }
+
+    public static int runProg6(String prog) {
+        int maxCountS = 10;
+
+        RobotWithSet k79 = new RobotWithSet(maxCountS, maxCountS, 0);
         int res = k79.runProgramUntilCross(prog);
         return res;
     }
---
+++ src/RobotWithSet.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class RobotWithSet {
+
+    Position pos;
+    int directionGrad;
+    int countS = 0;
+    //Хэшсет всегда работает, но будет работать неправильно, если не переопределен метод equals
+    Set<Position> history=new HashSet<>();
+    //Трисет выбрасывает исключение, если нет метода compareTo
+   // Set<Position> history=new TreeSet<>();
+
+    public RobotWithSet(int x, int y, int directionGrad) {
+        this.pos = new Position(x, y);
+        this.directionGrad = directionGrad;
+    }
+
+    public void turnLeft() {
+        directionGrad = (directionGrad + 90) % 360;
+    }
+
+    public void turnRight() {
+        directionGrad = (directionGrad - 90 + 360) % 360;
+    }
+
+    public void stepForward() {
+        history.add(pos);       //позиция ушла в историю
+        //переход на новую позицию:
+        if (directionGrad == 0)   pos = new Position(pos.x+1, pos.y);
+        if (directionGrad == 90)  pos = new Position(pos.x, pos.y+1);
+        if (directionGrad == 180) pos = new Position(pos.x-1, pos.y);
+        if (directionGrad == 270) pos = new Position(pos.x, pos.y-1);
+        countS++;
+    }
+
+    public void doCommand(char symbol) {
+        if (symbol == 'R')
+            turnRight();
+        if (symbol == 'L')
+            turnLeft();
+        if (symbol == 'S')
+            stepForward();
+    }
+
+    public boolean checkIfIWasHere() {
+        return history.contains(pos);
+    }
+
+    public int runProgramUntilCross(String proga) {
+        for (char symbol : proga.toCharArray()) {
+            doCommand(symbol);
+            if (symbol=='S' && checkIfIWasHere() )
+                return this.countS;
+        }
+        return -1;
+    }
+}
+
